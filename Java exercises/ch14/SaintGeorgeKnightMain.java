eada58ea2f58932f6420f0312a9b4293de55b453
M.Mavromanolakis <mauromanolakism@gmail.com>
2024-02-13 00:00:43 +0200

    ads

--- Java exercises/ch14/SaintGeorgeKnightMain.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.ch14;
-
-public class SaintGeorgeKnightMain {
-
-    public static void main(String[] args) {
-        SaintGeorgeKnight saintGeorge = SaintGeorgeKnight.getInstance();
-        saintGeorge.embarkOnMission();
-    }
-}
